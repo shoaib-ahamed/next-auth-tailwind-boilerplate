429ff188bd8175e4603447286a8a84d1f5e26962
shoaibnesoi <shoaib@nesoi.one>
2022-11-02 23:08:44 +0600

    next-auth & authentication some content change

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    "NEXTAUTH_URL" : "https://next-auth-tailwind-boilerplate.vercel.app/login",
+    "NEXTAUTH_URL" : "https://next-auth-tailwind-boilerplate.vercel.app",
     "MONGO_URL" : "mongodb+srv://shoaib:shoaib@nesoione.xlgdcpb.mongodb.net/?retryWrites=true&w=majority",
     "ACCESS_TOKEN_SECRET" : "shoaibahamed@gmail.com01672019292123412",
     "REFRESH_TOKEN_SECRET" : "shoaibahamed@gmail.com0167201929201303082197",
