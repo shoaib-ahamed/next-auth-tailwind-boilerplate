e2164b26f926d8ef4c85ce8bc7da2eb7d6f9038f
shoaibnesoi <shoaib@nesoi.one>
2022-11-21 02:04:14 +0600

    auth finish with sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+import { signOut } from 'next-auth/react';
+import React, { useState } from 'react';
+import { VscSignOut } from 'react-icons/vsc';
 
 
-const Sidebar = ({handleSignOut}) => {
+const Sidebar = () => {
     const [open , setOpen] = useState(true)
 
   return (
@@ -85,24 +87,11 @@ const Sidebar = ({handleSignOut}) => {
                                     <span className="hidden md:block">Settings</span>
                                 </a>
                             </li> */}
-                            <li className="flex items-center p-2 space-x-3 rounded-md" onClick={handleSignOut}>
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="w-6 h-6"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                        strokeWidth={2}
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
-                                        />
-                                    </svg>
-                                    <button type="button" onClick={handleSignOut}>
+                            <li>
+                                <button className="flex items-center p-2 space-x-3 rounded-md" type="button" onClick={() => signOut()}>
+                                    <VscSignOut className="text-2xl"/>
                                     <span className="hidden md:block">Logout</span>
-                                    </button>
+                                </button>
                             </li>
                         </ul>
                     </div>
